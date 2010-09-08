36e2d3d8bbeaa7ac5043d11c33e6a4ec4ce4454f
Noritaka Horio <holy.shared.design@gmail.com>
2010-09-08 22:03:09 +0900

    vCard changed.
    Signed-off-by: Noritaka Horio <holy.shared.design@gmail.com>

--- wp-content/themes/exhibition/about.php
+++ wp-content/themes/exhibition/about.php
@@ -20,7 +20,7 @@ Template Name: about
 				<h2><?php the_title(); ?></h2>
 			</div>
 			<div class="bd">
-				<?php the_content('<p class="serif"><strong>'.__("Read the whole story...", "grain").'</strong></p>'); ?>
+				<?php the_content('<p class="serif"><strong>'.__("Read the whole story...", "exhibition").'</strong></p>'); ?>
 			</div>
 		</div>
 	</div>
@@ -29,19 +29,17 @@ Template Name: about
 
 <div class="sidebar">
 		<div class="vcard">
-			<p class="me"><img class="photo" src="<?php bloginfo('template_directory'); ?>/images/noritaka_horio.jpg" /></p>
-			<h3><a class="fn n" href=""><span class="first-name">Noritaka</span>&nbsp;<span class="last-name">Horio</span></a></h3>
+			<p class="me"><?php echo get_avatar(get_the_author_meta("user_email"), $size = '96') ?></p>
+			<h3><a class="fn n" href=""><span class="first-name"><?php the_author_meta("first_name"); ?></span>&nbsp;<span class="last-name"><?php the_author_meta("last_name"); ?></span></a></h3>
 			<dl>
 				<dt>nickname:</dt>
-				<dd class="nickname">horry</dd>
-				<dt>organization:</dt>
-				<dd><a class="org" title="concrete5 japan" href="http://concrete5-japan.org/">concrete5 japan</a></dd>
-				<dt>title:</dt>
-				<dd class="title">member</dd>
+				<dd class="nickname"><?php the_author_meta("nickname"); ?></dd>
 				<dt>email:</dt>
-				<dd><a class="email" href="mailto:holy.shared.design@gmail.com">holy.shared.design@gmail.com</a></dd>
+				<dd><a class="email" href="mailto:<?php the_author_meta("user_email"); ?>"><?php the_author_meta("user_email"); ?></a></dd>
 				<dt>url:</dt>
-				<dd><a class="url" href="http://sharedhat.com">http://sharedhat.com</a></dd>
+				<dd><a class="url" href="<?php the_author_meta("user_url"); ?>"><?php the_author_meta("user_url"); ?></a></dd>
+				<dt>description:</dt>
+				<dd><?php the_author_meta("description"); ?></dd>
 			</dl>
 		</div>
 </div>
