8143a374209157571239a66c3048eb1376b57c50
Noritaka Horio <holy.shared.design@gmail.com>
2010-09-17 00:11:55 +0900

    bugixed....
    Signed-off-by: Noritaka Horio <holy.shared.design@gmail.com>

--- wp-content/themes/exhibition/searchform.php
+++ wp-content/themes/exhibition/searchform.php
@@ -1,12 +1,3 @@
-<!--<div class="mod searchForm">
-	<div class="inner">
-		<div class="hd"><h3>Search: </h3></div>
-		<div class="bd">
-		</div>
-	</div>
-</div>
-  -->
-
 <form method="get" action="<?php bloginfo('home'); ?>">
 	<fieldset>
 		<input type="text" name="s" id="s" size="19" />&nbsp;<input type="submit" value="<?php esc_attr_e('Search'); ?>" />
